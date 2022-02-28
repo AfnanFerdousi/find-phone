8827c0faa141076a5b7ff6ff94940bd88649d10a
AfnanFerdousi <afnanferdousi550@gmail.com>
2022-02-28 21:03:29 +0600

    search input done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('hhhhh');
+const loadPhones = () => {
+const searchValue = document.getElementById("search-box").value;
+
+const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+//   console.log(url);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
